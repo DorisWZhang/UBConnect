0f6e4b15cd0bad6b02070849781396028eed6c66
Doris Zhang <doriszhang912@gmail.com>
2025-01-18 13:05:58 -0800

    setting up pages

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,8 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Redirect } from 'expo-router';
+import React from 'react';
 
-export default function HomeScreen() {
-  return (
-    
-  );
+
+export default function Index() {
+
+  return <Redirect href="/landing" />;
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
--- app/landing.tsx
+++ app/landing.tsx
@@ -1,10 +1,58 @@
 import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
-import { Link } from 'expo-router';
+import { Link, useRouter } from 'expo-router';
+
+import React from 'react';
+
 export default function LandingPage() {
 
-
-
+    const handleClick = async () => {
+        router.push('/explore');
+    }
+ 
+    const router = useRouter(); // Create router instance to navigate
   return (
-    <Text> landing</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>welcome</Text>
+      <Link href="/login" asChild>
+        <TouchableOpacity style={styles.button} onPress={handleClick}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </Link>
+      <Link href="/signup" asChild>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Sign up</Text>
+        </TouchableOpacity>
+      </Link>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white'
+  },
+  title: {
+    fontSize: 30,
+    marginBottom: 20,
+    fontWeight: '500',
+    color: '#FF724C'
+  },
+  button: {
+    backgroundColor: '#FFC350',
+    height: 50,
+    width: 300,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 25,
+    marginTop: 25,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: '500',
+    fontFamily: 'Inter_500Medium',
+  },
+});
