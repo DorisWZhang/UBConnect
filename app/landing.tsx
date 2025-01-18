8015e9ef2d9b486247892047655b27e393067989
Doris Zhang <doriszhang912@gmail.com>
2025-01-18 12:43:24 -0800

    set up pages

---
+++ app/landing.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
+import { Link } from 'expo-router';
+export default function LandingPage() {
+
+
+
+  return (
+    <Text> landing</Text>
+  );
+}
