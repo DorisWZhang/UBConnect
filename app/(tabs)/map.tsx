613e255637c0360e10a56afa01988c830be0d83a
krish <karora07@student.ubc.ca>
2025-01-19 00:17:10 -0800

    Map added

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,21 +1,22 @@
-import { StyleSheet, Image, Platform, View, Text } from 'react-native';
-import { Link, useRouter } from 'expo-router';
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import React from 'react';
+"use client";
+
+import { Text } from 'react-native';
+import { Link, useRouter } from 'expo-router';;
+import React, {useState} from 'react';
+import { AdvancedMarker, APIProvider, InfoWindow, Map, Pin } from '@vis.gl/react-google-maps';
 
 export default function MapPage() {
 
-    const router = useRouter();
+  const position = { lat: 49.2606, lng: -123.2460 }; 
+  const [open, setOpen] = useState(false);
+
   return (
-    <View > 
-      <Text>
-      MAP PAGE
-      </Text>
-      </View>
+    <APIProvider apiKey='AIzaSyAyZ8Q7D-1RDQGbjRZxIApOAND_9ovuKhA'>
+      <div style={{ height: '100vh' }}>
+        <Map zoom={13} center={ position } mapId={ '5616403b42848ff7' }>
+          
+        </Map>
+      </div>
+      </APIProvider>
   );
 }
