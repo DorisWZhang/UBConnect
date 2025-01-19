caf97c4014cc0451d90a046c0986f259159c12cb
Doris Zhang <doriszhang912@gmail.com>
2025-01-18 18:15:45 -0800

    setting up firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,27 @@
-import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAopxRE2QKISJ8pkXXpsiNNSlEEqPKbZ2c",
+  authDomain: "ubconnect-793d3.firebaseapp.com",
+  projectId: "ubconnect-793d3",
+  storageBucket: "ubconnect-793d3.firebasestorage.app",
+  messagingSenderId: "505763970947",
+  appId: "1:505763970947:web:3bdb5a06d3fe387af500b6",
+  measurementId: "G-K1PY1QN1LP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app);
 // (Optional) import { getStorage } from 'firebase/storage';
 
 // Replace the config object with your actual Firebase credentials const firebaseConfig = { apiKey: "YOUR_API_KEY", authDomain: "YOUR_PROJECT_ID.firebaseapp.com", projectId: "YOUR_PROJECT_ID", storageBucket: "YOUR_PROJECT_ID.appspot.com", messagingSenderId: "YOUR_SENDER_ID", appId: "YOUR_APP_ID", };
